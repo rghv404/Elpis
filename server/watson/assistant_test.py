55a9efa27d5428cd6ed90382401fc030a17963cf
Dipack P Panjabi <dipack.panjabi@gmail.com>
2019-02-16 12:40:41 -0500

    Get working assistant request-response model

--- server/watson/assistant_test.py
+++ server/watson/assistant_test.py
@@ -10,4 +10,15 @@ if __name__ == "__main__":
 
     assistant_id = "0b6f795c-82f8-4315-9998-368560192535"
     session = assistant.create_session(assistant_id).get_result()
+    session_id = session["session_id"]
+    message = assistant.message(
+        assistant_id,
+        session_id,
+        input={'text': 'What\'s the weather like?'},
+        context={
+            'metadata': {
+                'deployment': 'myDeployment'
+            }
+        }).get_result()
+    print(json.dumps(message, indent=2))
     print(json.dumps(session))
